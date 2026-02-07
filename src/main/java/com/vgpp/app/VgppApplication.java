cea5103d70b45480f43c834f38bbf33df9ba86ec
Rashmi <Rashmi.Pingili23@gmail.com>
2026-02-07 17:15:49 -0600

    Initializing VGPP Spring Boot project

---
+++ src/main/java/com/vgpp/app/VgppApplication.java
@@ -0,0 +1,13 @@
+package com.vgpp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VgppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VgppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vgpp/app/VgppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vgpp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VgppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
